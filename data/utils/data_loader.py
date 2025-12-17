5321afbc8c5d08b3c5539e5abc7823bb2ce5f0a8
Alex <a.fgx2004@gmail.com>
2025-12-17 09:58:48 +0100

    fix: changed to one function load_data that loads the dataset without droping anythin

--- data/utils/data_loader.py
+++ data/utils/data_loader.py
@@ -1,13 +1,6 @@
 import pandas as pd
 
 
-def load_linear_regression():
+def load_data():
     df = pd.read_csv("data/CVD_cleaned_dummies.csv")
-    cols_to_remove = ["Height_(cm)", "Weight_(kg)"]
-    return df.drop(columns=cols_to_remove)
-
-
-def load_classification():
-    df = pd.read_csv("data/CVD_cleaned_dummies.csv")
-    cols_to_remove = ["Heart_Disease_Yes"]
-    return df.drop(columns=cols_to_remove)
+    return df
